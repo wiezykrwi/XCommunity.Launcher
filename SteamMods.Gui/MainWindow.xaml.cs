c40b07f58022789d1ccd36a3412f11bf370f5ee6
Wiezy Krwi <mathia.vandepoel@gmail.com>
2023-10-22 22:35:56 +0200

    add gui app

---
+++ SteamMods.Gui/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace SteamMods.Gui
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeButtonClicked(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
